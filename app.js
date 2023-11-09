91af8644c22597d58709708769dc7d355dfd2092
Sri Vasavi Peravarapu <s566425@nwmissouri.edu>
2023-11-08 22:14:49 -0600

    resource

--- app.js
+++ app.js
@@ -21,21 +21,7 @@ var resourceRouter = require('./routes/resource');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/organisation',organisationRouter);
-app.use('/board',boardRouter);
-app.use('/choose',chooseRouter);
-app.use('/resource',resourceRouter);
 
 async function recreateDB(){
   // Delete everything
@@ -64,10 +50,29 @@ async function recreateDB(){
  let reseed = true;
  if (reseed) {recreateDB();}
 
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/organisation',organisationRouter);
+app.use('/board',boardRouter);
+app.use('/choose',chooseRouter);
+app.use('/resource',resourceRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
-});
+}
+);
 
 // error handler
 app.use(function(err, req, res, next) {
--- controllers/organisation.js
+++ controllers/organisation.js
@@ -1,16 +1,50 @@
 var organisation = require('../models/organisation');
+
 // List of all Costumes
-exports.organisation_list = function(req, res) {
- res.send('NOT IMPLEMENTED: organisation list');
-};
+exports.organisation_list = async function(req, res) {
+    try{
+    theorganisation = await organisation.find();
+    res.send(theorganisation);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    } 
+   };
+
+//    exports.organisation_list = async function(req, res) {
+//     try{
+//     theorganisation = await organisation.find();
+//     res.send(theorganisation);
+//     }
+//     catch(err){
+//     res.status(500);
+//     res.send(`{"error": ${err}}`);
+//     } 
+//    };
+
+//    exports.organisation_list = async function(req, res) {
+//     try{
+//     theorganisation = await organisation.find();
+//     res.send(theorganisation);
+//     }
+//     catch(err){
+//     res.status(500);
+//     res.send(`{"error": ${err}}`);
+//     } 
+//    };
+// List of all Costumes
+// exports.organisation_list = function(req, res) {
+//  res.send('NOT IMPLEMENTED: organisation list');
+// };
 // for a specific Costume.
 exports.organisation_detail = function(req, res) {
  res.send('NOT IMPLEMENTED: organisation detail: ' + req.params.id);
 };
 // Handle Costume create on POST.
-exports.organisation_create_post = function(req, res) {
- res.send('NOT IMPLEMENTED: organisation create POST');
-};
+ exports.organisation_create_post = function(req, res) {
+  res.send('NOT IMPLEMENTED: organisation create POST');
+ };
 // Handle Costume delete form on DELETE.
 exports.organisation_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: organisation delete DELETE ' + req.params.id);
@@ -18,4 +52,4 @@ exports.organisation_delete = function(req, res) {
 // Handle Costume update form on PUT.
 exports.organisation_update_put = function(req, res) {
  res.send('NOT IMPLEMENTED: organisation update PUT' + req.params.id);
-}
+};
