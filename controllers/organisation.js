7e151d4e007b3e9530dde6f83423b678393c7d1e
Sri Vasavi Peravarapu <s566425@nwmissouri.edu>
2023-11-09 21:43:10 -0600

    lab

--- controllers/organisation.js
+++ controllers/organisation.js
@@ -1,26 +1,59 @@
 var organisation = require('../models/organisation');
-// List of all Costumes
+// List of all organisations
 // exports.organisation_list = function(req, res) {
 // res.send('NOT IMPLEMENTED: organisation list');
 // };
-// for a specific Costume.
-exports.organisation_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: organisation detail: ' + req.params.id);
-};
-// Handle Costume create on POST.
+// for a specific organisation.
+// for a specific organisation.
+exports.organisation_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await organisation.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+// Handle organisation create on POST.
 // exports.organisation_create_post = function(req, res) {
 // res.send('NOT IMPLEMENTED: organisation create POST');
 // };
-// Handle Costume delete form on DELETE.
+// Handle organisation delete form on DELETE.
 exports.organisation_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: organisation delete DELETE ' + req.params.id);
 };
-// Handle Costume update form on PUT.
-exports.organisation_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: organisation update PUT' + req.params.id);
+// Handle organisation update form on PUT.
+
+
+// Handle organisation update form on PUT.
+exports.organisation_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await organisation.findById( req.params.id)
+// Do updates of properties
+if(req.body.organisation_type)
+toUpdate.organisation_type = req.body.organisation_type;
+if(req.body.cost) toUpdate.cost = req.body.cost;
+if(req.body.size) toUpdate.size = req.body.size;
+
+if(req.body.checkboxsale) toUpdate.sale = true;
+else toUpdate.same = false;
+
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
 };
+
+
  
-// List of all Costumes
+// List of all organisations
 exports.organisation_list = async function(req, res) {
 try{
 theorganisation = await organisation.find();
@@ -45,14 +78,14 @@ exports.organisation_view_all_Page = async function(req, res) {
     }
     };
    
-// Handle Costume create on POST.
+// Handle organisation create on POST.
 exports.organisation_create_post = async function(req, res) {
     console.log(req.body)
     let document = new organisation();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costume_type":"goat", "cost":12, "size":"large"}
+    // {"organisation_type":"goat", "cost":12, "size":"large"}
     document.course = req.body.course;
     document.faculty = req.body.faculty;
     document.section = req.body.section;
--- routes/organisation.js
+++ routes/organisation.js
@@ -1,6 +1,10 @@
 var express = require('express');
 const organisation_controlers= require('../controllers/organisation');
 var router = express.Router();
-/* GET costumes */
+/* GET organisations */
 router.get('/', organisation_controlers.organisation_view_all_Page );
+
+// GET request for one organisation.
+router.get('/organisations/:id', organisation_controlers.organisation_detail);
+
 module.exports = router
